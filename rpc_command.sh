f49ffeb65431ed218c375203ac7cbb37fd3c3aea
shengxianshui <shengxianshui@iMorpheus.io>
2019-01-23 15:58:11 +0800

    Submit send raw tx cmd

--- rpc_command.sh
+++ rpc_command.sh
@@ -16,3 +16,6 @@
 #curl --data-binary '{"jsonrpc": "2.0", "id":"1", "method": "tau_accounts", "params": [] }'  http://127.0.0.1:8606
 curl --data-binary '{"jsonrpc": "2.0", "id":"1", "method": "tau_newaccount", "params": [] }'  http://127.0.0.1:8606
 #curl --data-binary '{"jsonrpc": "2.0", "id":"1", "method": "tau_importprikey", "params": [70375058633862235424321040800080181186779250603075941646381306644335108412868] }'  http://127.0.0.1:8606
+
+#send raw transaction
+#curl --data-binary '{"jsonrpc": "2.0", "id":"1", "method": "tau_sendTransaction", "params": [{"to":"0xabcdefabcdefabcdefab", "value": 10000, "fee": 100, "privkey":"0xd25ed9f789974c6c5c7e7204bfcc5a458f2e3484d52b0eab93b5fd014edf705b"}] }'  http://127.0.0.1:8606/
