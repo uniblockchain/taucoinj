aa2ef34e79ebb6e469f8d5b15ccfb02e2059ee68
shengxianshui <shengxianshui@iMorpheus.io>
2019-02-26 19:54:07 +0800

    Optimize gap recovery logic

--- taucoinj-core/src/main/java/io/taucoin/sync/SyncQueue.java
+++ taucoinj-core/src/main/java/io/taucoin/sync/SyncQueue.java
@@ -69,6 +69,8 @@ public class SyncQueue {
     @Autowired
     private MapDBFactory mapDBFactory;
 
+    private byte[] genesisBlockHash = null;
+
     /**
      * Loads HashStore and BlockQueue from disk,
      * starts {@link #produceQueue()} thread
@@ -128,7 +130,10 @@ public class SyncQueue {
 //                    logger.info("No parent on the chain for block.number: {} block.hash: {}", wrapper.getNumber(), wrapper.getBlock().getShortHash());
                     wrapper.importFailed();
                     syncManager.tryGapRecovery(wrapper);
-                    blockQueue.add(wrapper);
+                    // Here not add this block into block queue, because we don't
+                    // know its block number. This node will request block headers
+                    // and block bodies ASAP.
+                    //blockQueue.add(wrapper);
                     noParent = true;
                     sleep(2000);
                 } else {
@@ -341,7 +346,7 @@ public class SyncQueue {
         List<BlockHeader> newHeaders = new ArrayList<BlockHeader>();
         List<BlockHeader> filtered = blockQueue.filterExistingHeaders(headers);
 
-        for (BlockHeader header : headers) {
+        for (BlockHeader header : filtered) {
 
             if (!isValid(header)) {
 
@@ -354,10 +359,9 @@ public class SyncQueue {
             } else {
                 newHeaders.add(header);
             }
-
         }
 
-        if (fillupHeaderNumber(headers)) {
+        if (fillupHeaderNumber(newHeaders)) {
             if (logger.isDebugEnabled()) {
                for (BlockHeader header : headers) {
                    logger.debug("compute header number {}:{}",
@@ -365,9 +369,8 @@ public class SyncQueue {
                            header.getNumber());
                }
             }
-            headerStore.addBatch(headers);
+            headerStore.addBatch(newHeaders);
         }
-
 
         if (logger.isDebugEnabled())
             logger.debug("{} headers filtered out, {} added", headers.size() - filtered.size(), filtered.size());
@@ -501,6 +504,7 @@ public class SyncQueue {
         if (headers == null || headers.size() == 0) {
             return false;
         }
+        initGenesisBlockHash();
 
         Block firstParent = this.blockchain.getBlockByHash(headers.get(0).getPreviousHeaderHash());
         Block lastParent = this.blockchain.getBlockByHash(headers.get(headers.size() - 1).getPreviousHeaderHash());
@@ -509,10 +513,18 @@ public class SyncQueue {
         long startBlockNumber  = 0;
         long delta;
 
-        if (firstParentNumber >= 0 && lastParentNumber == -1) {
+        if (isGenesisBlockHash(headers.get(0).getHash())) {
+            // If first header is genesis block header, the start block number is 0.
+            startBlockNumber = 0;
+            delta = 1;
+        } else if (isGenesisBlockHash(headers.get(headers.size() - 1).getHash())) {
+            // If last header is genesis block header
+            startBlockNumber = headers.size() - 1;
+            delta = -1;
+        } else if (firstParentNumber >= 0 && lastParentNumber == -1) {
             startBlockNumber = firstParentNumber + 1;
             delta = 1;
-        } else if (lastParentNumber >= 0 && firstParentNumber != -1) {
+        } else if (lastParentNumber >= 0 && firstParentNumber == -1) {
             startBlockNumber = lastParentNumber + headers.size();
             delta = -1;
         } else if (firstParentNumber >= 0 && lastParentNumber >= 0) {
@@ -536,5 +548,16 @@ public class SyncQueue {
         }
 
         return true;
+    }
+
+    private void initGenesisBlockHash() {
+        if (this.genesisBlockHash == null) {
+            Block genesisBlock = this.blockchain.getBlockByNumber(0);
+            this.genesisBlockHash = genesisBlock.getHash();
+        }
+    }
+
+    private boolean isGenesisBlockHash(byte[] hash) {
+        return Arrays.equals(genesisBlockHash, hash);
     }
 }
