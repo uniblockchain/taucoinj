567cba2fce8c2bee783fb180417cd26a43e23daf
hankelc <taichen.liu@imorpheus.io>
2019-02-27 10:42:46 +0800

    add close method in main method

--- taucoinj-core/src/main/java/io/taucoin/Start.java
+++ taucoinj-core/src/main/java/io/taucoin/Start.java
@@ -11,36 +11,74 @@ import org.spongycastle.util.encoders.Hex;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
 import static io.taucoin.config.SystemProperties.CONFIG;
 
 /**
- * @author Roman Mandeleil
- * @since 14.11.2014
- */
+ *  * @author Roman Mandeleil
+ *   * @since 14.11.2014
+ *    */
 public class Start {
-
-    public static void main(String args[]) throws IOException, URISyntaxException {
-        CLIInterface.call(args);
-
-        if (!CONFIG.blocksLoader().equals("")) {
-            CONFIG.setSyncEnabled(false);
-            CONFIG.setDiscoveryEnabled(false);
-        }
-
-        Taucoin taucoin = TaucoinFactory.createTaucoin();
-
-        if (!CONFIG.blocksLoader().equals(""))
-            taucoin.getBlockLoader().loadBlocks();
-
-        // Start rpc server
-        if (CONFIG.isRpcEnabled()) {
-            JsonRpcServer rpcServer = JsonRpcServerFactory.createJsonRpcServer(taucoin);
+    private static final Logger logger = LoggerFactory.getLogger("main");
+    private static Taucoin taucoin;
+    private static class TauSignalHandler implements SignalHandler {
+    
+        @Override
+        public void handle(Signal signal) {
+            logger.info("Signal handler called for signal {}", signal);
             try {
-                rpcServer.start(CONFIG.rpcListenPort());
+                taucoin.close();
+                System.exit(0);
+                logger.info("Handling {}", signal.getName());
             } catch (Exception e) {
+                logger.error("handle|Signal handler" + "failed, reason "
+                + e.getMessage());
                 e.printStackTrace();
             }
-        }
+        }   
     }
 
+    public static void main(String args[]) {
+
+        SignalHandler handler = new TauSignalHandler();
+        // kill (-15)
+        Signal termSignal = new Signal("TERM");
+        Signal.handle(termSignal, handler);
+        // kill (-2) and ctrl+c
+        Signal intSignal = new Signal("INT");
+        Signal.handle(intSignal, handler);
+
+        try{
+
+            CLIInterface.call(args);
+
+            if (!CONFIG.blocksLoader().equals("")) {
+                CONFIG.setSyncEnabled(false);
+                CONFIG.setDiscoveryEnabled(false);
+            }
+
+            taucoin = TaucoinFactory.createTaucoin();
+
+            if (!CONFIG.blocksLoader().equals(""))
+                taucoin.getBlockLoader().loadBlocks();
+
+            // Start rpc server
+            if (CONFIG.isRpcEnabled()) {
+                JsonRpcServer rpcServer = JsonRpcServerFactory.createJsonRpcServer(taucoin);
+                try {
+                    rpcServer.start(CONFIG.rpcListenPort());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Interrupted: " + e.getMessage());
+        }
+
+    }
 }
