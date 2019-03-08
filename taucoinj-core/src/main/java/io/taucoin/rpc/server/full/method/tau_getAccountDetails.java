8a97ba00623ebf87e4513e4b574802aee3dce0f2
hankelc <taichen.liu@imorpheus.io>
2019-03-08 14:14:03 +0800

    modify account's detail, for T*

--- taucoinj-core/src/main/java/io/taucoin/rpc/server/full/method/tau_getAccountDetails.java
+++ taucoinj-core/src/main/java/io/taucoin/rpc/server/full/method/tau_getAccountDetails.java
@@ -2,6 +2,7 @@ package io.taucoin.rpc.server.full.method;
 import com.thetransactioncompany.jsonrpc2.*;
 import com.thetransactioncompany.jsonrpc2.server.*;
 import io.taucoin.core.AccountState;
+import io.taucoin.core.VersionedChecksummedBytes;
 import io.taucoin.db.BlockStore;
 import io.taucoin.facade.Repository;
 import io.taucoin.rpc.server.full.JsonRpcServerMethod;
@@ -17,25 +18,33 @@ import java.util.List;
 import java.util.Map;
 
 public class tau_getAccountDetails extends JsonRpcServerMethod {
+
     public tau_getAccountDetails(Taucoin taucoin){
         super(taucoin);
     }
 
     //todo
     protected JSONRPC2Response worker(JSONRPC2Request req, MessageContext ctx) {
+
         Repository repo = taucoin.getRepository();
         List<Object> params = req.getPositionalParams();
+
         if (params.size() < 1 ) {
             return new JSONRPC2Response(JSONRPC2Error.INVALID_PARAMS, req.getID());
         }
+
         ArrayList<String> result = new ArrayList<String>();
-        System.out.println("address is: "+ Hex.toHexString(jsToByteArray((String)params.get(0))));
-        if(repo.isExist(jsToByteArray((String)params.get(0)))){
-            System.out.println("have come here");
-            result.add("address: "+ Hex.toHexString(jsToByteArray((String)params.get(0))));
-            result.add("balance: "+repo.getBalance(jsToByteArray((String)params.get(0))).toString());
-            result.add("forgepower: "+repo.getforgePower(jsToByteArray((String)params.get(0))).toString());
-            AccountState ac = repo.getAccountState(jsToByteArray((String)params.get(0)));
+        byte[] address = null;
+        VersionedChecksummedBytes toEncoedAddress= new VersionedChecksummedBytes((String) params.get(0));
+        address = toEncoedAddress.getBytes();
+
+        if(repo.isExist(address)){
+
+            result.add("address: "+ Hex.toHexString(address));
+            result.add("balance: "+repo.getBalance(address).toString());
+            result.add("forgepower: "+repo.getforgePower(address).toString());
+
+            AccountState ac = repo.getAccountState(address);
             Iterator iterator = ac.getTranHistory().entrySet().iterator();
             int i=0;
             while(iterator.hasNext()) {
@@ -45,6 +54,7 @@ public class tau_getAccountDetails extends JsonRpcServerMethod {
                 i++;
             }
         }
+
         JSONRPC2Response res = new JSONRPC2Response(result, req.getID());
         return res;
 
