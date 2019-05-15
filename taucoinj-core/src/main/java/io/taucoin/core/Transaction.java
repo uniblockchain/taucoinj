b55bbef54c9ab41ada36e4400857f3d048a02456
shunli.wang <shunli.wang@dwfund.net>
2019-05-15 16:40:21 +0800

    repair problem that parse transaction

--- taucoinj-core/src/main/java/io/taucoin/core/Transaction.java
+++ taucoinj-core/src/main/java/io/taucoin/core/Transaction.java
@@ -105,6 +105,7 @@ public class Transaction {
         } else {
             this.rlpEncodedComposite = rawData;
         }
+        this.isCompositeTx = isComposite;
         parsed = false;
     }
     /* creation tx
